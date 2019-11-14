3039e9f17e4cc508a5c26c01f30f63a7564f8ac8
Aayush Gupta <aayushgupta2097@gmail.com>
2019-11-14 12:28:05 +0530

    adding timeout to locust

--- locustfile.py
+++ locustfile.py
@@ -6,16 +6,23 @@ class MyTaskSet(TaskSet):
     # def my_task(self):
     #     print('executing task')
 
-    @task(3)
+    @task(6)
     def task1(self):
         self.client.get("/")
 
-    @task(6)
+    @task(9)
     def task2(self):
         self.client.get("/list_data")
+
+    @task(3)
+    def task3(self):
+        self.client.get("/fibonacci")
 
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
     min_wait = 5000
     max_wait = 15000
+    stop_timeout = 1200
+
+# locust --host=http://localhost:8000
