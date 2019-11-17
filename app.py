f7d7246bf4681c39f32e695b63ab1e72323a9435
Aayush Gupta <aayushgupta2097@gmail.com>
2019-11-17 20:01:48 +0530

    cleaning app.py

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ from walrus import *
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#Database
+# Database
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:igdefault@localhost:5432/flaskapi'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://owner_username:password@db_host:5432/db_name'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -132,60 +133,22 @@ def computation_task():
 
 @app.route('/data', methods=['GET'])
 def data_api():
-    requests.get("http://dummy.restapiexample.com/api/v1/employees")
-    requests.get("http://dummy.restapiexample.com/api/v1/employees")
-    requests.get("http://dummy.restapiexample.com/api/v1/employees")
-    requests.get("http://dummy.restapiexample.com/api/v1/employees")
-    requests.get("http://dummy.restapiexample.com/api/v1/employees")
-    requests.get("http://dummy.restapiexample.com/api/v1/employees")
-    requests.get("http://dummy.restapiexample.com/api/v1/employees")
-    requests.get("http://dummy.restapiexample.com/api/v1/employees")
-    return "Hello"
-    #
-    # r = requests.get("http://dummy.restapiexample.com/api/v1/employees")
-    # for i in range(1, 5):
-    #     list_data = r.json()
-    #     for z in range(8):
-    #         list_data.extend(list_data)
-    #     # data = jsonify(r.json())
-    #     # print(r)
-    #     # print(r.json())
-    #
-    #     count = 10
-    #     for d in list_data:
-    #         while count > 0:
-    #             for k, v in d.items():
-    #                 pass
-    #             count = count - 1
-    # # print(dict_data)
-    # data = jsonify(list_data)
-    # return data
+    return jsonify(requests.get("http://dummy.restapiexample.com/api/v1/employees").json())
 
 
 @app.route('/list_data', methods=['GET'])
 # @Cache.cached(Cache, timeout=60)
 def new_api():
-    # r = requests.get("http://dummy.restapiexample.com/api/v1/employees")
-    # list_data = r.json()
-    # for num in range(0, 8):
-    #     list_data.extend(list_data)
-    # print(len(list_data))
-    # return "Hi"\
-
-    # print(json.loads(response.text))
-    # json_data = response.json() if response and response.status_code == 200 else None
-    # print(jsonify(response.json()))
-    # print(type(response.json()))
-    # list_data = r.json()
-    # print(type(list_data))
-    # data = jsonify(list_data)
-    # print(data)
-    return jsonify(requests.get("https://jsonplaceholder.typicode.com/comments").json())
+    r = requests.get("https://jsonplaceholder.typicode.com/comments")
+    list_data = r.json()
+    for num in range(0, 8):
+        list_data.extend(list_data)
+    return jsonify(list_data)
+    # return jsonify(requests.get("https://jsonplaceholder.typicode.com/comments").json())
 
 
 @app.route('/fibonacci', methods=['GET'])
 def get_fibonacci():
-
     series_length = 10000
     first = 0
     second = 1
