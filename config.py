c58ac45b5f8624a6811c18eb6cc71ab5d9eb28bd
Aayush Gupta <aayushgupta2097@gmail.com>
2019-11-21 09:59:33 +0530

    clean gunicorn config file

--- config.py
+++ config.py
@@ -3,26 +3,23 @@ import multiprocessing
 bind = 'localhost:8000'
 backlog = 2048
 
-# workers = 9
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 9
+# workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'sync'
+# threads = 3
 worker_connections = 1000
 timeout = 30
 keepalive = 2
 
 spew = False
-
-
 daemon = False
 
-pidfile = None
-umask = 0
 user = None
 group = None
 tmp_upload_dir = None
 
 errorlog = '-'
-loglevel = 'info'
+# loglevel = 'critical'
 accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
@@ -31,3 +28,7 @@ proc_name = None
 
 # gunicorn -c config.py wsgi:app
 # gunicorn -w8 wsgi:app
+
+# without request logging
+# gunicorn -b :8091 -w 1 -k gevent --worker-connections=2000 --backlog=1000 -p gunicorn.pid --log-level=critical rse:app
+# 40 200 product
